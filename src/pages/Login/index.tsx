1a2dd48f823c0d1c3354133088007208e1def272
alimarzoug12 <marzougali@isimsf.u-sfax.tn>
2022-08-08 17:49:43 +0100

    refactor: review logic in `login page` index component

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,18 +1,34 @@
 import React from 'react'
 
-export function Login() {
-  return (
-    <form>
-    Login : <br/>
-    <label>Nom :  
-    <input type="text" placeholder='user name'/>
-    </label><br/>
-    <label>Mot de passe :  
-    <input type="password" placeholder='password'/>
-    </label><br/>
-    <input type="submit" value="Envoyer"></input>
+export function Login():JSX.Element {
     
-    </form>
+    const [userName,setUserName]=React.useState<string>("")
+    const [password,setPassword]=React.useState<string>("")
+
+   function onclickLoginHandleEvent(userName:string,password:string)
+   {  
+    if(userName === "ali" && password==="123")
+    console.log("bonjour")
+    
+    else console.log("salut")
+}
+
+  return (
+        <div  style={{gap:"30px"}}>
+    <h1>Login </h1>
+    <div className="d-felx">
+        <span>Nom :  </span>
+    <input type="text" placeholder='userName' value={userName} name="nom"
+     onChange={(event)=>setUserName(event.target.value)}/>
+    </div>
+    <div className='d-felx'>
+        <span>Mot de passe : </span>  
+    <input type="password" placeholder='password' name="password" pattern="8" 
+    value={password} onChange={(event)=>setPassword(event.target.value)}/>
+    </div>
+    <button type="submit" onClick={()=>{onclickLoginHandleEvent(userName,password)}}> valider</button>
+</div>
+  
   )
 }
 
