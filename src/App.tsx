c42d399bcd7b98bb538b8a9e73fd5e77ec8797bd
alimarzoug12 <marzougali@isimsf.u-sfax.tn>
2022-08-11 10:24:52 +0100

    feat: add `routes` in `app` component

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,25 @@
 import React from 'react';
+import {BrowserRouter,Route, Routes} from 'react-router-dom'
 
-
-import { Test } from './pages/Test';
+import { Login } from './pages/Login';
+import { Home } from './pages/Home';
+import { Register } from './pages/Register';
+import { Profile } from './pages/Profile';
 
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-     <Test/>
+    <div className="App-header" style={{background:"#003249"}}>
+     
+    <BrowserRouter>
+    <Routes>
+    <Route path="/" element={<Home/>}/>
+    <Route path="/login" element={<Login/>}/>
+    <Route path="/register" element={<Register/>}/>
+    <Route path="/profile" element={<Profile/>}/>
+    </Routes>
+    </BrowserRouter>
     </div>
   );
 }
