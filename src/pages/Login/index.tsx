2119a042c7a5c8b16ff353452d62c3678b3c643f
alimarzoug12 <marzougali@isimsf.u-sfax.tn>
2022-08-11 15:46:42 +0100

    refactor

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -3,33 +3,32 @@ import { useNavigate } from "react-router-dom";
 
 export function Login():JSX.Element {
     
-    const [userName,setUserName]=React.useState<string>("")
+    const [email,setEmail]=React.useState<string>("")
     const [password,setPassword]=React.useState<string>("")
 
     const navigate = useNavigate();
-   function onclickLoginHandleEvent(userName:string,password:string)
+   function onclickLoginHandleEvent(email:string,password:string)
    {  
-    if(userName === "ali" && password==="123")
-    console.log("bonjour") 
-    else console.log("salut")
-    navigate("/profile")
-    }
+    if(email!=="" && password!=="")
+    {
+        navigate("/profile")}
+}
 
   return (
         <div  style={{gap:"30px"}}>
     <h1>Login </h1>
     <div className="lgn">
     <div className="d-flex">
-        <span>Nom  </span></div><div>
-    <input type="text" className="input" placeholder='User Name' value={userName} name="nom"
-     onChange={(event)=>setUserName(event.target.value)}/>
+        <span>Email  </span></div><div>
+    <input type="email" className="input" placeholder='Email' value={email} name="email"
+     onChange={(event)=>setEmail(event.target.value)}/>
     </div>
     <div className='d-flex'>
         <span>Mot de passe </span> </div><div> 
     <input type="password" className="input" placeholder='Password' name="password" pattern="8" 
     value={password} onChange={(event)=>setPassword(event.target.value)}/>
     </div></div>
-    <button type="submit" className="button" onClick={()=>{onclickLoginHandleEvent(userName,password)}}>Valider</button>
+    <button type="submit" className="button" onClick={()=>{onclickLoginHandleEvent(email,password)}}>Valider</button>
 </div>
   
   )
